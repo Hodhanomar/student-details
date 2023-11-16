bc9491107332ad0d45ab324d73b512134580e35e
Hodhanomar <151074350+Hodhanomar@users.noreply.github.com>
2023-11-16 23:23:27 +0300

    Add files via upload

---
+++ Studentdetails.java
@@ -0,0 +1,17 @@
+public class Studentdetails {
+   public static void main(String[]args) {
+     String course= "ENGINEERING";
+     String name="HODHAN OMAR";
+     String reg="22/07875";
+     String dob="7/1/2013";
+     String vision="HIGH REGARDS";
+     String favlang= "JAVA";
+
+     System.out.println("Course:"+ course);
+     System.out.println("Name:" + name);
+     System.out.println("Registration Number :" + reg);
+     System.out.println("Date of Birth:"+ dob );
+     System.out.println("Vision:" + vision );
+     System.out.println("Favourite Programming Language:" + favlang);
+  }
+}
